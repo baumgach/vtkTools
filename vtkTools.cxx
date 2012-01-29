243d731c03e3f8cc208d0de3cbbf038cc69bbe35
Chrigi <baumgach@ee.ethz.ch>
2012-01-29 11:37:32 -0500

    changes to main file

--- vtkTools.cxx
+++ vtkTools.cxx
@@ -61,21 +61,7 @@ int main(int argc, char* argv[]) {
 
   //std::cout << "max fiber length: " << nMaxFiberLength << std::endl;
 
-  for (int j=0; j<nMaxFiberLength; j++) {
-    for (int i=0; i<in_fibers.size(); ++i) {
-      if (j<in_fibers[i].Points.size()) {
-        std::cout << in_fibers[i].Points[j]._[0] << ",";
-        std::cout << in_fibers[i].Points[j]._[1] << ",";
-        std::cout << in_fibers[i].Points[j]._[2];
-      } else {
-        std::cout << " , , ";
-      }
-      // only add the last colon if its not the end of the line
-      if (i != in_fibers.size()-1)
-        std::cout << ",";
-    }
-    std::cout << std::endl;
-  }
+
   
   return 0;
 
